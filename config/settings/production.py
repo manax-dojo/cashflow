f280acde4017a8b853295a5ed0dab593e35618c6
Bruno Santeramo <bruno.santeramo@gmail.com>
2017-05-25 14:38:58 +0200

    anymail

--- config/settings/production.py
+++ config/settings/production.py
@@ -98,3 +98,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Some really nice defaults
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
+# ------------------------------------------------------------------------------ 
+# /AUTHENTICATION CONFIGURATION 
+ 
+# EMAIL 
+# ------------------------------------------------------------------------------ 
+INSTALLED_APPS += ("anymail", ) 
+DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL', 
+                         default='example <noreply@example.com>') 
+SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL) 
+EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[example] ') 
+ 
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"  # or sendgrid.EmailBackend, or... 
+ 
+# Anymail with Mailgun 
+ANYMAIL = { 
+    "MAILGUN_API_KEY": env('DJANGO_MAILGUN_API_KEY'), 
+    "MAILGUN_SENDER_DOMAIN": env('DJANGO_MAILGUN_SENDER_DOMAIN') 
+} 
+# ------------------------------------------------------------------------------ 
+# /EMAIL 
